ac85cb560dadb1b69694f68d1e31cce0e51c9d6a
Yuta Takahashi <y.highbridge04@gmail.com>
2025-03-29 22:06:15 +0900

    fix: Bind HTTP server to localhost for improved security

--- test-https-server/runserver.py
+++ test-https-server/runserver.py
@@ -9,7 +9,7 @@ Handler = SimpleHTTPRequestHandler
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain(CERTFILE)
 
-with HTTPServer(("", PORT), Handler) as httpd:
+with HTTPServer(("127.0.0.1", PORT), Handler) as httpd:
     print("serving at address", httpd.server_address, "using cert file", CERTFILE)
     httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
     httpd.serve_forever()
